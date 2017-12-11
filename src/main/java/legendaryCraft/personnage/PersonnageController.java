4374a068074270c22281cd6a6742fc79aca25a13
duthoitl <lucile.duthoit@etudiant.univ-lille1.fr>
2017-12-11 09:18:49 +0100

    début du controller d'un personnage

--- src/main/java/legendaryCraft/personnage/PersonnageController.java
+++ src/main/java/legendaryCraft/personnage/PersonnageController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import legendaryCraft.item.Item;
 
 @Controller
 @AutoConfigureDataMongo
@@ -23,5 +23,12 @@ public class PersonnageController {
 		List<Personnage> personnages = repository.findAll();
 		model.addAttribute("personnages", personnages);
 		return "personnages";
+	}
+	
+	@RequestMapping("/personnage/{id}")
+	public String personnage(@PathVariable String id, Model model) {
+		Personnage personnage = repository.findById(id);
+		model.addAttribute("personnage", personnage);
+		return "personnage";
 	}
 }
