95fcbcd159e67ea89da286c1c020a5e3772f3f8f
Dorian Ongenae <dorian.ongenae@etudiant.univ-lille1.fr>
2017-12-13 12:10:58 +0100

    item : plus de slots d'items mais deux booleans, un vérifiant si l'item est une arme (pour faciliter le travail sur le personnage) et l'autre s'il est à deux mains (ainsi si l'item est une arme on vérifie ce champs pour savoir)

--- src/main/java/legendaryCraft/item/ItemRepository.java
+++ src/main/java/legendaryCraft/item/ItemRepository.java
@@ -2,16 +2,13 @@ package legendaryCraft.item;
 
 import java.util.List;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import legendaryCraft.item.enums.ItemType;
 import legendaryCraft.item.enums.Rarete;
-import legendaryCraft.item.enums.Slot;
 
 @Repository
-@Order(2)
 public interface ItemRepository extends MongoRepository<Item, String>  {
 	
 	public Item findByNom (String nom);
@@ -23,7 +20,4 @@ public interface ItemRepository extends MongoRepository<Item, String>  {
 	public List<Item> findByRarete(Rarete rarete);
 	
 	public List<Item> findByItemTypeAndRarete(ItemType type, Rarete rarete);
-	
-	// ne marche pas, on verra plus tard
-	public List<Item> findBySlots(List<Slot> slots);
 }
